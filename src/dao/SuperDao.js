c4ced99c25809ec7baf6f5f2379bd90155166cdb
Kazi Naimul Hoque <90389165+aoyan107@users.noreply.github.com>
2022-10-03 11:06:30 +0600

    Update SuperDao.js

--- src/dao/SuperDao.js
+++ src/dao/SuperDao.js
@@ -1,14 +1,30 @@
+const logger = require('../config/logger');
+
 class SuperDao {
     constructor(model) {
         this.Model = model;
     }
 
     async findAll() {
-        return this.Model.findAll();
+        return this.Model.findAll()
+            .then((result) => {
+                return result;
+            })
+            .catch((e) => {
+                logger.error(e);
+                console.log(e);
+            });
     }
 
     async findById(id) {
-        return this.Model.findOne({ where: { id } });
+        return this.Model.findOne({ where: { id } })
+            .then((result) => {
+                return result;
+            })
+            .catch((e) => {
+                logger.error(e);
+                console.log(e);
+            });
     }
 
     async findOneByWhere(where, attributes = null, order = ['id', 'desc']) {
@@ -16,26 +32,66 @@ class SuperDao {
             return this.Model.findOne({
                 where,
                 order: [order],
-            });
+            })
+                .then((result) => {
+                    return result;
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    console.log(e);
+                });
         }
         return this.Model.findOne({
             where,
             attributes,
             order: [order],
-        });
+        })
+            .then((result) => {
+                return result;
+            })
+            .catch((e) => {
+                logger.error(e);
+                console.log(e);
+            });
     }
 
     async updateWhere(data, where) {
-        return this.Model.update(data, { where });
+        return this.Model.update(data, { where })
+            .then((result) => {
+                return result;
+            })
+            .catch((e) => {
+                logger.error(e);
+                console.log(e);
+            });
     }
 
     async updateById(data, id) {
-        return this.Model.update(data, { where: { id } });
+        return this.Model.update(data, { where: { id } })
+            .then((result) => {
+                return result;
+            })
+            .catch((e) => {
+                logger.error(e);
+                console.log(e);
+            });
     }
 
     async create(data) {
-        const newData = new this.Model(data);
-        return newData.save();
+        try {
+            const newData = new this.Model(data);
+            return newData
+                .save()
+                .then((result) => {
+                    return result;
+                })
+                .catch((e) => {
+                    logger.error(e);
+                    console.log(e);
+                });
+        } catch (e) {
+            console.log(e);
+        }
     }
 
     async findByWhere(
@@ -68,11 +124,25 @@ class SuperDao {
     }
 
     async bulkCreate(data) {
-        return this.Model.bulkCreate(data);
+        return this.Model.bulkCreate(data)
+            .then((result) => {
+                return result;
+            })
+            .catch((e) => {
+                logger.error(e);
+                console.log(e.message);
+            });
     }
 
     async getCountByWhere(where) {
-        return this.Model.count({ where });
+        return this.Model.count({ where })
+            .then((result) => {
+                return result;
+            })
+            .catch((e) => {
+                logger.error(e);
+                console.log(e);
+            });
     }
 
     async incrementCountInFieldByWhere(fieldName, where, incrementValue = 1) {
@@ -91,6 +161,43 @@ class SuperDao {
         }
         // eslint-disable-next-line no-return-await
         return await instance.decrement(fieldName, { by: decrementValue });
+    }
+
+    async updateOrCreate(values, condition) {
+        return this.Model.findOne({ where: condition }).then((obj) => {
+            // update
+            if (obj) {
+                return obj.update(values);
+            }
+            // insert
+            return this.Model.create(values);
+        });
+    }
+
+    async checkExist(condition) {
+        return this.Model.count({ where: condition }).then((count) => {
+            if (count !== 0) {
+                return true;
+            }
+            return false;
+        });
+    }
+
+    async getDataTableData(where, limit, offset, order = [['id', 'DESC']]) {
+        return this.Model.findAndCountAll({
+            limit: parseInt(limit, 10),
+            offset: parseInt(offset, 10),
+            where,
+            order,
+        })
+            .then((result) => {
+                return result;
+            })
+            .catch((e) => {
+                logger.error(e);
+                console.log(e);
+                return [];
+            });
     }
 }
 module.exports = SuperDao;
